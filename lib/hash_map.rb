9622dc42e339d26805ca8cb3d7bacbee3aae4ae9
Steve Purcell <steve.purcell@powershop.co.nz>
2015-06-22 10:20:35 +1200

    Store multiple pairs, but all in one big bucket

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -1,18 +1,23 @@
 class HashMap
+  def initialize
+    @storage = []
+  end
+
   def size
     0
   end
 
   def has_key?(key)
-    @key == key
+    @storage.any? { |k, v| k == key }
   end
 
   def [](key)
-    @value
+    if entry = @storage.find { |k, v| k == key }
+      entry[1]
+    end
   end
 
   def []=(key, value)
-    @key = key
-    @value = value
+    @storage << [key, value]
   end
 end
--- spec/hash_map_spec.rb
+++ spec/hash_map_spec.rb
@@ -20,9 +20,19 @@ RSpec.describe HashMap do
 
     it "allows insertion of a key/value pair" do
       expect(map["key"]).to eql(nil)
-      map["key"] = "value"
+      returned = (map["key"] = "value")
+      expect(returned).to eql("value")
       expect(map.has_key?("key")).to eql(true)
       expect(map["key"]).to eql("value")
+    end
+
+    it "allows insertion of multiple key/value pairs" do
+      map["key1"] = "value1"
+      map["key2"] = "value2"
+      expect(map.has_key?("key1")).to eql(true)
+      expect(map["key1"]).to eql("value1")
+      expect(map.has_key?("key2")).to eql(true)
+      expect(map["key2"]).to eql("value2")
     end
   end
 
